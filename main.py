6ab670a5eef3c5a8888c0a260d2abf49feab4492
Neha Deshmukh <135820790+nehakdeshmukh@users.noreply.github.com>
2023-06-28 21:49:20 -0300

    Update main.py
    commented a function

--- main.py
+++ main.py
@@ -408,7 +408,7 @@ print("total_time: ",total_time)
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
